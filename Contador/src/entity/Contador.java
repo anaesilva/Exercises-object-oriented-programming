57e4154a785f2d6b644b55b43c24eca9236ead3f
Ana Silva <annadasilva89@hotmail.com>
2023-09-13 17:49:50 -0300

    test

---
+++ Contador/src/entity/Contador.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Contador {
+
+    private static int cont;
+
+    public Contador() {
+        cont++;
+    }
+
+    public static void incrementar(){
+        cont++;
+    }
+
+    public static void zerar(){
+        cont = 0;
+    }
+
+    public static int obterValor(){
+        return cont;
+    }
+}
--- Lampada/src/Main.java
+++ Lampada/src/Main.java
@@ -4,17 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         Lampada lampada = new Lampada();
-        lampada.modelo = "A60";
-        lampada.tensao = "Bivolt";
-        lampada.garantiaMeses = 36;
-        lampada.potencia = 7;
-        lampada.cor = "Amarela";
-        lampada.tipoLuz = "Amarela";
-        lampada.tipoAbajur = true;
-
-        lampada.tipos = new String[5];
-        lampada.tipos[0] = "Abajur";
-        lampada.tipos[1] = "Lampeões";
 
         lampada.ligar();
 
